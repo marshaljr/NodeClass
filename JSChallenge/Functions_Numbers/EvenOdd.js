420dfce5de72ffea8680e14f6edc359b954566e0
Marshal Ram <84084013+marshaljr@users.noreply.github.com>
2026-06-26 08:41:50 +0545

    even-odd-check

--- JSChallenge/Functions_Numbers/EvenOdd.js
+++ JSChallenge/Functions_Numbers/EvenOdd.js
@@ -1,2 +1,15 @@
 // 7. Even or Odd Checker
 // Create a function that checks whether a given number is even or odd.
+
+function evenOrOdd(num) {
+  if (num % 2 === 0) {
+    return "Even";
+  } else {
+    return "Odd";
+  }
+}
+console.log(evenOrOdd(8)); // Even
+console.log(evenOrOdd(15)); // Odd
+console.log(evenOrOdd(0)); // Even
+console.log(evenOrOdd(-6)); // Even
+console.log(evenOrOdd(-7)); // Odd
