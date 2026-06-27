262bc718fb11c7a5c77b60441df5f34e2e7b3563
Marshal Ram <84084013+marshaljr@users.noreply.github.com>
2026-06-28 02:53:43 +0545

    refactor: factorial

--- JSChallenge/Functions_Numbers/Factorial.js
+++ JSChallenge/Functions_Numbers/Factorial.js
@@ -4,18 +4,19 @@
 function factorial(n) {
   if (n < 0) {
     throw new Error("Factorial is not defined for negative numbers.");
-  } else if (n === 0 || n === 1) {
-    return 1;
-  } else {
-    let result = 1;
-    for (let i = 2; i <= n; i++) {
-      result *= i;
-    }
-    return result;
   }
+  if (n === 0 || n === 1) {
+    return 1;
+  }
+  let result = 1;
+  for (let i = 2; i <= n; i++) {
+    result *= i;
+  }
+  return result;
 }
 console.log(factorial(5)); // 120
 console.log(factorial(4)); // 24
 console.log(factorial(0)); // 1
 console.log(factorial(1)); // 1
-console.log(factorial(-1)); // Error: Factorial is not defined for negative numbers.
+// console.log(factorial(-1));
+// Error: Factorial is not defined for negative numbers.
